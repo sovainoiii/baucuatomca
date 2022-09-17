d99d6df2b5988992b1cd2ad6834a3cc66ad748f6
sovainoiii <100119038+sovainoiii@users.noreply.github.com>
2022-09-17 11:36:03 +0700

    v0.0.3

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const http = require('http').Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
